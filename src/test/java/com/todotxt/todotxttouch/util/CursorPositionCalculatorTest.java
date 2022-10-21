efcc2bb4d6831a08aea0928237bc5b7bec7b4b09
Duarte Figueiredo <duarte.figueiredo@outlook.com>
2022-10-21 21:04:16 +0100

    Fixed unit tests for CursorPositionCalculator.java

--- src/test/java/com/todotxt/todotxttouch/util/CursorPositionCalculatorTest.java
+++ src/test/java/com/todotxt/todotxttouch/util/CursorPositionCalculatorTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @RunWith(Parameterized.class)
@@ -32,22 +33,22 @@ public class CursorPositionCalculatorTest {
     @Parameterized.Parameters(name = "{index}: calculate({0},\"{1}\",\"{2}\") = {3}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {-1, null, null, 0},
-                {-1, null, EMPTY_STRING, 0},
-                {-1, null, BLANK_STRING, 5},
-                {-1, null, SOME_VALUE, 10},
-                {-1, EMPTY_STRING, null, 0},
-                {-1, EMPTY_STRING, EMPTY_STRING, 0},
-                {-1, EMPTY_STRING, BLANK_STRING, 4},
-                {-1, EMPTY_STRING, SOME_VALUE, 9},
-                {-1, BLANK_STRING, null, 0},
-                {-1, BLANK_STRING, EMPTY_STRING, 0},
-                {-1, BLANK_STRING, BLANK_STRING, 0},
-                {-1, BLANK_STRING, SOME_VALUE, 4},
-                {-1, SOME_VALUE, null, 0},
-                {-1, SOME_VALUE, EMPTY_STRING, 0},
-                {-1, SOME_VALUE, BLANK_STRING, 0},
-                {-1, SOME_VALUE, SOME_VALUE, 0},
+                {-1, null, null, Exception.class},
+                {-1, null, EMPTY_STRING, Exception.class},
+                {-1, null, BLANK_STRING, Exception.class},
+                {-1, null, SOME_VALUE, Exception.class},
+                {-1, EMPTY_STRING, null, Exception.class},
+                {-1, EMPTY_STRING, EMPTY_STRING, Exception.class},
+                {-1, EMPTY_STRING, BLANK_STRING, Exception.class},
+                {-1, EMPTY_STRING, SOME_VALUE, Exception.class},
+                {-1, BLANK_STRING, null, Exception.class},
+                {-1, BLANK_STRING, EMPTY_STRING, Exception.class},
+                {-1, BLANK_STRING, BLANK_STRING, Exception.class},
+                {-1, BLANK_STRING, SOME_VALUE, Exception.class},
+                {-1, SOME_VALUE, null, Exception.class},
+                {-1, SOME_VALUE, EMPTY_STRING, Exception.class},
+                {-1, SOME_VALUE, BLANK_STRING, Exception.class},
+                {-1, SOME_VALUE, SOME_VALUE, Exception.class},
                 {0, null, null, 0},
                 {0, null, EMPTY_STRING, 0},
                 {0, null, BLANK_STRING, 5},
@@ -69,8 +70,12 @@ public class CursorPositionCalculatorTest {
 
     @Test
     public void test() {
-        var actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
-        assertThat(actual).isEqualTo(expected);
+        if (expected instanceof Integer) {
+            var actual = CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue);
+            assertThat(actual).isEqualTo(expected);
+        } else {
+            assertThrows(Exception.class,
+                         () -> CursorPositionCalculator.calculate(priorCursorPosition, priorValue, newValue));
+        }
     }
-
 }
